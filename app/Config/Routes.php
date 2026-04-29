4269c80b620b673eaf043f489858e76231d8df7a
NRMiaro <miarotools@gmail.com>
2026-04-29 12:13:39 +0300

    new controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,32 +1,12 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\EtudiantController;
 
 /**
  * @var RouteCollection $routes
  */
 
-$routes->get('/login', 'AuthController::index');
-
-
-$routes->get('/', 'LivresController::index');
-
-$routes->group('livres', function($routes) {
-    
-    // /livres
-    $routes->get('/', 'LivresController::index');            
-    
-    //  /livre/voir/1
-    $routes->get('voir/(:num)', 'LivresController::view/$1');  
-
-    
-    $routes->get('creer', 'LivresController::create');       
-    $routes->post('stocker', 'LivresController::store');     
-
-    // /livre/supprimer/1
-    $routes->post('supprimer/(:num)', 'LivresController::delete/$1'); 
-    
-    //  Emprunts et Retours
-    $routes->post('emprunter/(:num)', 'Emprunts::sortir/$1');
-    $routes->post('rendre/(:num)', 'Emprunts::retour/$1');
+$routes->group("etudiants", function($routes){
+    $routes->get("/", "EtudiantController::liste");
 });
